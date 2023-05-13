268b393151c2bebbc2522e043616acc9440fb39e
Ihnatsi Rakutska <ihnatsirakutska@192.168.1.8>
2023-05-13 13:13:51 +0200

    Second exercise solution

--- exercise.py
+++ exercise.py
@@ -8,4 +8,16 @@ def most_often_letter(string):
             ans_letter = letter
     return f'{ans_letter} - {max_numb}'
 
+
+
+def one_list(list_in):
+    ans_list = []
+    for sublist in list_in:
+        if isinstance(sublist, list):
+            one_list(sublist)
+        else:
+            ans_list.append(sublist)
+     return ans_list
+
 print(most_often_letter("blaldaaaaaaaaaaaaflkshjkdfhsla"))
+print(one_list([1, 2, 3, [4, 5], 6, [[[7]]], 8, 9, 10]))
